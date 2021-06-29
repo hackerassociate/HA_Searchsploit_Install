eb86e2486344037aa3e5088c8c63a3e102f6b5f4
hackerassociate <62351511+hackerassociate@users.noreply.github.com>
2021-06-29 20:45:44 +0530

    Create HA_searchsploit_install.sh

---
+++ HA_searchsploit_install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+echo -e "\n
+-----------------------------------------------------------------------------------------------------
+			        Welcome to Offensive IOT by Harshad!
+                            
+              Harsh BOT Ready for Vulnerability Searching. 
+					
+			        #Author = Harshad Shah
+                   
+			    
+                  
+-----------------------------------------------------------------------------------------------------"
+
+
+echo -e "\nWelcome to Offensive IOT  Program!\n"
+
+echo -e "\nH-Bot is going to configure searchsploit in your system!\n"
+
+echo -e  "\n\n" ;
+
+sudo git clone https://github.com/offensive-security/exploitdb.git /opt/exploitdb
+
+sudo ln -sf /opt/exploitdb/searchsploit /usr/local/bin/searchsploit
+
+
+echo -e  "\n\n" ;
+
+searchsploit -h
+
+echo -e  "\n\n" ;
+
+searchsploit -u
+
